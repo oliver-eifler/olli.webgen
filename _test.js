a15f32b9b8cf1970a1c30898badb51d5b6b25ee7
Oliver Jean Eifler <olli.eifler@gmail.com>
2016-05-29 19:35:48 +0200

    yaml metadata
    Draft usage of YAML metadata and lodash templates

--- _test.js
+++ _test.js
@@ -2,8 +2,10 @@
  * Created by darkwolf on 22.05.2016.
  */
 var plugin = "./index.js";
-var webgen = require(plugin);
+var webgen = require("./index.js");
 var doc = new webgen("test/test.olli.html",{cheerio:{decodeEntities:false},
     plugins:{"minify":{},"image":{imagePath:"test/",forceFrame:true}}});
 doc.process();
 doc.save("test/final.html");
+
+
