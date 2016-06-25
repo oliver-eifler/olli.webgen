69a7bf8a783ab9be99c86bd500635342553f8dd3
Oliver Jean Eifler <olli.eifler@gmail.com>
2016-06-25 21:44:53 +0200

    only PHP timestamp
    Unix Timestamp without milliseconds

--- test/final.php
+++ test/final.php
@@ -7,7 +7,77 @@ $title = $page->title = "Oliver Jean Eifler";
 $subtitle = $page->subtitle = "Programmierer. Techniker. Künstler.";
 $description = $page->description = "Um was geht's hier eigentlich bla bla hallo\n";
 ob_start();
-?><div class="content-container baseline"><div class="hero-container bumper"><div class="hero"><h1 class="text-smart"><span>Oliver Jean Eifler</span></h1><h3 class="text-smart hug"><span>Programmierer. Techniker. Künstler.</span></h3><p>Um was geht's hier eigentlich bla bla hallo </p><p><small>May 22, 2016 June 16, 2016 8:40 PM</small></p></div></div><div class="content content-width"><div class="box pic center" data-reflow="" style="max-width: 960px;"><div class="box-dummy" style="padding-bottom: 59.89583%;"></div><iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" class="box-embed" data-src="images/welpe.jpg"></iframe><noscript><img class="box-embed" src="images/welpe.jpg"></noscript><div class="caption"><div class="caption-text">Picture: a nice pic of my dog</div></div></div><p><i data-icon-embed="" class="icon-invader"></i> Erinnerungen, <a href="#">Prophezeiungen</a>, Fantasiegespinste und <a href="#">Liebe</a>, Vergangenheit, Zukunft und der Augenblick des Traums dazwischen -<br>Sie alle schaffen ein Land, das einen einzigen, unsterblichen Tag lang existiert.</p><p>Das zu wissen, ist <a href="#">Weisheit</a>. Das zu nutzen, ist <a href="#"><strong>'Kunst'</strong></a>. <i data-icon-embed="" class="icon-invader"></i></p><figure data-reflow=""><div class="box pic center" style="max-width: 560px;"><div class="box-dummy" style="padding-bottom: 150%;"></div><iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" class="box-embed" data-src="images/faultier.jpg"></iframe><noscript><img class="box-embed" src="images/faultier.jpg"></noscript></div><figcaption style="display:block;max-width:400px;margin:0 auto"><cite>Depression</cite>. By: Darren Harris</figcaption></figure><p> Memory, prophecy, and fantasy— The past, the future, and The dreaming moment between— Are all in one country, Living one immortal day. </p><p>To know that is Wisdom. To use it is the Art.</p><div class="box pic center" data-reflow="" style="max-width: 320px;"><div class="box-dummy" style="padding-bottom: 78.125%;"></div><iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" class="box-embed" data-src="http://lorempixel.com/320/250/?v=<?= rand(1, 99) ?>"></iframe><noscript><img class="box-embed" src="http://lorempixel.com/320/250/?v=<?= rand(1, 99) ?>"></noscript></div><div class="box pic center" data-reflow="" style="max-width: 640px;"><div class="box-dummy" style="padding-bottom: 75%;"></div><iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" class="box-embed" data-src="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>"></iframe><noscript><img class="box-embed" src="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>"></noscript></div><div class="box pic center" data-reflow="" style="max-width: 640px;"><div class="box-dummy" style="padding-bottom: 75%;"></div><iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" class="box-embed" data-src="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>"></iframe><noscript><img class="box-embed" src="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>"></noscript></div><div class="box pic center" data-reflow="" style="max-width: 320px;"><div class="box-dummy" style="padding-bottom: 150%;"></div><iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" class="box-embed" data-src="http://lorempixel.com/320/480/?v=<?= rand(1, 99) ?>"></iframe><noscript><img class="box-embed" src="http://lorempixel.com/320/480/?v=<?= rand(1, 99) ?>"></noscript></div></div></div><?php
+?>
+    <div class="content-container baseline">
+    <div class="hero-container bumper">
+        <div class="hero"><h1 class="text-smart"><span>Oliver Jean Eifler</span></h1>
+            <h3 class="text-smart hug"><span>Programmierer. Techniker. Künstler.</span></h3>
+            <p>Um was geht's hier eigentlich bla bla hallo </p>
+            <p>
+                <small>May 22, 2016 June 16, 2016 8:40 PM</small>
+            </p>
+        </div>
+    </div>
+    <div class="content content-width" style="max-width: 320px;">
+        <div class="box pic center" data-reflow="" style="max-width: 960px;">
+            <div class="box-dummy" style="padding-bottom: 59.89583%;"></div>
+            <iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" width="960px" height="575px"
+                    class="box-embed" data-src="images/welpe.jpg"></iframe>
+            <noscript><img class="box-embed" src="images/welpe.jpg"></noscript>
+            <div class="caption"><a href="images/welpe.jpg" class="caption-text">Picture: a nice pic of my dog</a></div>
+        </div>
+        <p><i data-icon-embed="" class="icon-invader"></i> Erinnerungen, <a href="#">Prophezeiungen</a>,
+            Fantasiegespinste und <a href="#">Liebe</a>, Vergangenheit, Zukunft und der Augenblick des Traums dazwischen
+            -<br>Sie alle schaffen ein Land, das einen einzigen, unsterblichen Tag lang existiert.</p>
+        <p>Das zu wissen, ist <a href="#">Weisheit</a>. Das zu nutzen, ist <a href="#"><strong>'Kunst'</strong></a>. <i
+                data-icon-embed="" class="icon-invader"></i></p>
+        <figure data-reflow="" style="max-width: 560px;">
+            <div class="box pic center" style="max-width: 560px;">
+                <div class="box-dummy" style="padding-bottom: 150%;"></div>
+                <iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" width="560px"
+                        height="840px" class="box-embed" data-src="images/faultier.jpg"></iframe>
+                <noscript><img class="box-embed" src="images/faultier.jpg"></noscript>
+                <div class="caption"><a href="images/faultier.jpg" class="caption-text">images/faultier.jpg</a></div>
+            </div>
+            <figcaption style="display:block;max-width:400px;margin:0 auto"><cite>Depression</cite>. By: Darren Harris
+            </figcaption>
+        </figure>
+        <p> Memory, prophecy, and fantasy— The past, the future, and The dreaming moment between— Are all in one
+            country, Living one immortal day. </p>
+        <p>To know that is Wisdom. To use it is the Art.</p>
+        <div class="box pic center" data-reflow="" style="max-width: 320px;">
+            <div class="box-dummy" style="padding-bottom: 78.125%;"></div>
+            <iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" width="320px" height="250px"
+                    class="box-embed" data-src="http://lorempixel.com/320/250/?v=<?= rand(1, 99) ?>"></iframe>
+            <noscript><img class="box-embed" src="http://lorempixel.com/320/250/?v=<?= rand(1, 99) ?>"></noscript>
+            <div class="caption"><a href="http://lorempixel.com/320/250/?v=<?= rand(1, 99) ?>" class="caption-text">http://lorempixel.com/320/250/?v=<?= rand(1, 99) ?></a>
+            </div>
+        </div>
+        <div class="box pic center" data-reflow="" style="max-width: 640px;">
+            <div class="box-dummy" style="padding-bottom: 75%;"></div>
+            <iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" width="640px" height="480px"
+                    class="box-embed" data-src="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>"></iframe>
+            <noscript><img class="box-embed" src="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>"></noscript>
+            <div class="caption"><a href="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>" class="caption-text">http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?></a>
+            </div>
+        </div>
+        <div class="box pic center" data-reflow="" style="max-width: 640px;">
+            <div class="box-dummy" style="padding-bottom: 75%;"></div>
+            <iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" width="640px" height="480px"
+                    class="box-embed" data-src="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>"></iframe>
+            <noscript><img class="box-embed" src="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>"></noscript>
+            <div class="caption"><a href="http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?>" class="caption-text">http://lorempixel.com/640/480/?v=<?= rand(1, 99) ?></a>
+            </div>
+        </div>
+        <div class="box pic center" data-reflow="" style="max-width: 320px;">
+            <div class="box-dummy" style="padding-bottom: 150%;"></div>
+            <iframe scrolling="no" data-type="img" allowtransparency="true" frameborder="0" width="320px" height="480px"
+                    class="box-embed" data-src="http://lorempixel.com/320/480/?v=<?= rand(1, 99) ?>"></iframe>
+            <noscript><img class="box-embed" src="http://lorempixel.com/320/480/?v=<?= rand(1, 99) ?>"></noscript>
+            <div class="caption"><a href="http://lorempixel.com/320/480/?v=<?= rand(1, 99) ?>" class="caption-text">http://lorempixel.com/320/480/?v=<?= rand(1, 99) ?></a>
+            </div>
+        </div>
+    </div></div><?php
 $page->html = ob_get_contents();
 ob_end_clean();
 ?>
